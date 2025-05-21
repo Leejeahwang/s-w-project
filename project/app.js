367edb610a2d4d9f7d89392da84c79cfe8a9c69e
Jae_Hwang <woghkd1102@gmail.com>
2025-05-21 10:51:16 +0900

    프로토타입

--- project/app.js
+++ project/app.js
@@ -1,22 +1,28 @@
-const fs = require('fs');
-const http = require('http');
+const express = require('express');
+const path = require('path');
+const postRouter = require('./routes/post');
 
-const sever = http.createServer((req, res) => {
-    if(req.url === '/' && req.method === 'GET') {
-        fs.readFile('./index.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-            res.end(err ? '페이지 로드 오류' : data);
-        });
-    }
+const app = express();
 
-    else if(req.url === '/note_detail' && req.method === 'GET') {
-        fs.readFile('./note_detail.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-            res.end(err ? '페이지 로드 오류' : data);
-        });
-    }
+// 뷰 엔진 설정
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// 요청 Body 파싱
+app.use(express.urlencoded({ extended: false }));
+
+// 라우터 연결
+app.use('/posts', postRouter);
+app.get('/', (req, res) => res.redirect('/posts'));
+
+// 에러 핸들링
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('서버 오류 발생');
 });
 
-sever.listen(3000, () => {
-    console.log('http://localhost:3000 에서 서버 실행 중');
+// 서버 실행
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
