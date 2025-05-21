40381ad116812d3a85ee441e9a98be05562a0c7b
kk698334 <kk698334@gmail.com>
2025-05-21 21:23:39 +0900

    구조 간략화

--- 프로젝트/app.js
+++ 프로젝트/app.js
@@ -39,4 +39,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`)); 
